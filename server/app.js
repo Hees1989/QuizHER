cfaf0c49ca465e02b48fed368a85ab3d010d403f
Hees1989 <Hees1989@users.noreply.github.com>
2019-05-31 16:21:11 +0200

    added ws server

--- server/app.js
+++ server/app.js
@@ -1,13 +1,25 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const https = require('https');
+const WebSocket = require('ws');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+let app = express();
+
+// view websocket setup
+const server = https.createServer(app);
+const wss = new WebSocket.Server({server});
+
+wss.on('connection', function connection(ws) {
+  ws.send('Connected!');
+});
+
+server.listen(8080);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
