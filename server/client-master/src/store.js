735f356dc2174c9dfcecfebaa5483c3c96253088
Hees1989 <Hees1989@users.noreply.github.com>
2019-06-02 19:56:38 +0200

    added constants and installed socket middleware

--- server/client-master/src/store.js
+++ server/client-master/src/store.js
@@ -1,12 +1,15 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux';
+//import reduxWebsocket from '@giantmachines/redux-websocket';
 import logger from 'redux-logger';
 
 import user from './reducers/userReducer';
+
+//const reduxWebsocketMiddleware = reduxWebsocket();
 
 export default createStore(
    combineReducers({
        user
    }),
     {},
-    applyMiddleware(logger)
+    applyMiddleware(/*reduxWebsocketMiddleware, */logger)
 );
