1512f1fce4d4f4f5b15a605d3d4e8476fb1e2b70
Hees1989 <Hees1989@users.noreply.github.com>
2019-06-26 00:18:27 +0200

    commentfest
    alles wat niet gebruikt wordt uitgecomment

--- server/client-team/src/actions/answerActions.js
+++ server/client-team/src/actions/answerActions.js
@@ -1,36 +1,36 @@
-import {answerConstants} from "../constants/constants";
-import {questionConstants} from "../constants/constants";
-
-export const applyAnswer = (answer) => {
-    return {
-        type: answerConstants.APPLY_ANSWER,
-        payload: answer
-    }
-};
-
-export const resetAnswer = () => {
-    return {
-        type: answerConstants.RESET_ANSWER
-    }
-};
-
-export const appliedAnswer = () => {
-    return {
-        type: answerConstants.APPLIED_ANSWER,
-        payload:true
-    }
-};
-
-export const changedAnswer = () =>{
-    return {
-        type:answerConstants.CHANGE_ANSWER
-    }
-};
-
-export const setQuestion = (question) => {
-    return {
-        type: "SET_QUESTION",
-        payload: question
-    }
-};
-
+// import {answerConstants} from "../constants/constants";
+// import {questionConstants} from "../constants/constants";
+//
+// export const applyAnswer = (answer) => {
+//     return {
+//         type: answerConstants.APPLY_ANSWER,
+//         payload: answer
+//     }
+// };
+//
+// export const resetAnswer = () => {
+//     return {
+//         type: answerConstants.RESET_ANSWER
+//     }
+// };
+//
+// export const appliedAnswer = () => {
+//     return {
+//         type: answerConstants.APPLIED_ANSWER,
+//         payload:true
+//     }
+// };
+//
+// export const changedAnswer = () =>{
+//     return {
+//         type:answerConstants.CHANGE_ANSWER
+//     }
+// };
+//
+// export const setQuestion = (question) => {
+//     return {
+//         type: "SET_QUESTION",
+//         payload: question
+//     }
+// };
+//
--- server/client-team/src/store.js
+++ server/client-team/src/store.js
@@ -2,16 +2,16 @@ import {createStore, combineReducers, applyMiddleware} from 'redux';
 //import reduxWebsocket from '@giantmachines/redux-websocket';
 import logger from 'redux-logger';
 
-import user from './reducers/teamReducer';
-import answer from './reducers/answerReducer';
+//import user from './reducers/teamReducer';
+//import answer from './reducers/answerReducer';
 import team from './reducers/teamReducer';
 
 //const reduxWebsocketMiddleware = reduxWebsocket();
 
 export default createStore(
    combineReducers({
-       user,
-       answer,
+       //user,
+       //answer,
        team
    }),
     {},
