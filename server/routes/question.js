63e93ebb6f2e72659a49b58ed0274fd0babc89f3
Botan Abak <Botanabak@gmail.com>
2019-06-26 13:05:14 +0200

    update question

--- server/routes/question.js
+++ server/routes/question.js
@@ -6,21 +6,47 @@ const Questions = require('../model/question');
 
 //Bedoeling hiervan is dat je met 1 array 3 categorieen meegeeft
 
-router.get('/randomQuestions/:category1/:category2/:category3',(req,res) => {
+router.get('/randomQuestions/:category1/:category2/:category3', (req, res) => {
     // Questions.find({category:{$in:['Music','Sport','History']}}).then(
-        Questions.find({category:{$in:[req.params.category1,req.params.category2,req.params.category3]}}).then(
+    Questions.find({
+        category: {$in: [req.params.category1, req.params.category2, req.params.category3]},
+        used: false
+    }).then(
         questions => {
             const randomQuestions = [];
             const numberOfQuestions = 2;
             //const numberOfQuestions = 12;
             for (let i = 0; i < numberOfQuestions; i++) {
-                const random = Math.ceil((questions.length - 1) * Math.random() );
+                const random = Math.ceil((questions.length - 1) * Math.random());
                 randomQuestions[i] = questions[random];
             }
             res.json(randomQuestions)
         }
     )
 });
+
+
+
+router.post('/setUsed/:question', (req, res) => {
+    
+    let query = {'question':req.params.question};
+    Questions.findOneAndUpdate(query, {used:true}, {upsert:false}, function(err, doc){
+        if (err) return res.send(500, { error: err });
+        return res.send("succesfully saved");
+    });
+        // try {
+        //     Questions.updateOne()(
+        //         {question: req.params.question},
+        //         {$set:{gangsta: true}}
+        //         )
+        //
+        // } catch (e) {
+        //     print(e.());
+        // }
+
+        // res.json('oh yeah')
+    }
+);
 
 router.get('/allCategories', (req, res) => {
     Questions.distinct('category', (err, docs) => {
